04fce2473fe1a1e2dbc536d4f7506aba62dd3a1f
Jonathan Speiser <jspeis@gmail.com>
2021-06-14 21:01:07 -0400

    adds code for initial proof-of-concept

---
+++ sandbox/helpers.py
@@ -0,0 +1,90 @@
+import os
+import math
+import numpy as np
+from scipy import ndimage
+from PIL import Image
+
+HOUNSFIELD_MIN = -1000
+HOUNSFIELD_MAX = 2000
+HOUNSFIELD_RANGE = HOUNSFIELD_MAX - HOUNSFIELD_MIN
+
+
+def build_mask(pixels_array):
+    mask = np.zeros((512, 512), np.uint8)
+    for x, y in pixels_array:
+        mask[x][y] = 1
+    mask = ndimage.binary_fill_holes(mask).astype(int)
+    return mask
+
+
+def contour_to_pixels(contour_coord, ds):
+    x0 = contour_coord[len(contour_coord) - 3]
+    y0 = contour_coord[len(contour_coord) - 2]
+    coord = []
+    for i in range(0, len(contour_coord), 3):
+        # get the (x,y) coordinate, ignore Z
+        x = contour_coord[i]
+        y = contour_coord[i + 1]
+        # compute the length from x,y to x0,y0
+        # we do this to fill in the pixels from the contour points
+        l = math.sqrt((x - x0)**2 + (y - y0)**2)
+        # round up length * 2, and add 1 to ensure all points in between
+        # are filled in
+        l = math.ceil(l * 2) + 1
+        # add interpolated points
+        for j in range(1, l + 1):
+            coord.append([(x - x0) * j / l + x0, (y - y0) * j / l + y0])
+        # set the comparison points for next loop
+        x0 = x
+        y0 = y
+
+    x_spacing, y_spacing = float(ds.PixelSpacing[0]), float(ds.PixelSpacing[1])
+    origin_x, origin_y, _origin_z = ds.ImagePositionPatient
+
+    pixels = [(np.rint((y - origin_y) / y_spacing), np.rint((x - origin_x) / x_spacing)) for x, y in coord]
+    pixels = [(int(x), int(y)) for x, y in list(set(pixels))]
+    return pixels
+
+
+def fetch_contour_sop_instance_uid(metadata, uid):
+    structures = {item.ROINumber: item.ROIName for item in metadata.StructureSetROISequence}
+    roi_seq = metadata.ROIContourSequence
+    for roi in roi_seq:
+        if structures[roi.ReferencedROINumber] == 'Lung_L': # lung L
+            print('ROI Name:', structures[roi.ReferencedROINumber], roi.ReferencedROINumber)
+            contour_seq = roi.ContourSequence
+            for c in contour_seq:
+                if c.ContourImageSequence[0].ReferencedSOPInstanceUID == uid:
+                    print("OK!")
+                    return c
+
+
+def save_image_array(img_arr, target_path):
+    img = Image.fromarray(img_arr.astype(np.uint8))
+    img.save(target_path)
+    return img
+
+
+def normalize_intensity(img):
+    img[img < HOUNSFIELD_MIN] = HOUNSFIELD_MIN
+    img[img > HOUNSFIELD_MAX] = HOUNSFIELD_MAX
+    return (img - HOUNSFIELD_MIN) / HOUNSFIELD_RANGE
+
+
+def save_images_to_files(plt, subdir, imgarr, contour_mask, dicom_file, output_dir = 'test-output'):
+    masks_dir = os.path.join(output_dir, 'masks', 'lung_l')
+    images_dir = os.path.join(output_dir, 'images', 'lung_l')
+    plt.axis('off')
+    plt.imshow(contour_mask, cmap="gray")
+    if not os.path.exists(masks_dir):
+        os.makedirs(masks_dir)
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+    plt.savefig(f"{masks_dir}/{subdir}-{dicom_file}.png",
+        bbox_inches='tight', pad_inches = 0)
+    plt.clf()
+    plt.axis('off')
+    plt.imshow(imgarr, cmap="gray")
+    plt.savefig(f"{images_dir}/{subdir}-{dicom_file}.png",
+        bbox_inches='tight', pad_inches = 0)
+    plt.clf()
---
+++ sandbox/process.py
@@ -0,0 +1,64 @@
+'''
+Reads DICOM medical imaging files and outputs rasterized representations
+suitable for ML training purposes
+'''
+
+import os
+import click
+import logging
+import matplotlib.pyplot as plt
+from pydicom import dcmread
+
+from helpers import build_mask, normalize_intensity
+from helpers import contour_to_pixels, save_images_to_files
+from helpers import fetch_contour_sop_instance_uid
+
+
+def run(base_path, subdir, save_images=False, mode='training'):
+    metadata = dcmread(os.path.join(base_path, subdir, 'metadata', '1-1.dcm'))
+    num_with_rois = 0
+
+    img_dir = os.path.join(base_path, subdir, 'images')
+
+    for dicom_file in os.listdir(img_dir):
+        print(dicom_file)
+        full_dicom_path = os.path.join(img_dir, dicom_file)
+
+        dicom_img = dcmread(full_dicom_path)
+        sop_uid = dicom_img.SOPInstanceUID
+        imgarr = dicom_img.pixel_array
+        contour_obj = fetch_contour_sop_instance_uid(metadata, sop_uid)
+
+        if not contour_obj:
+            # if slice has no regions of interest, skip it for now
+            logging.info("Image file at index %s has no ROI data, skipping...", dicom_file)
+            continue
+
+        num_with_rois += 1
+        contour_coord = contour_obj.ContourData
+        contour_pixels = contour_to_pixels(contour_coord, dicom_img)
+        contour_mask = build_mask(contour_pixels)
+
+        imgarr = normalize_intensity(imgarr)
+
+        if save_images:
+            save_images_to_files(plt, subdir, imgarr, contour_mask, dicom_file, f'{mode}-output')
+
+    logging.info("Total images with ROIs %s, %s", num_with_rois, (num_with_rois/130.) * 100)
+
+
+
+@click.command()
+@click.option('--mode', default='training', help='Training or testing processing mode.')
+def process_images(mode):
+    base_dir = './lungs'
+    img_folder_base = os.path.join(base_dir, f'{mode}-data')
+    subdirs = os.listdir(img_folder_base)
+    for target_subdir in [d for d in subdirs if os.path.isdir(os.path.join(img_folder_base, d))]:
+        dataset_path = os.path.join(img_folder_base, target_subdir)
+        logging.info("Running process on %s ...", dataset_path)
+        run(dataset_path, target_subdir, save_images=True, mode=mode)
+
+
+if __name__ == '__main__':
+    process_images()
